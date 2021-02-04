c5ab527c2edda203a17d43a721288671b016adb0
AnnamalaiSathappan <77261223+AnnamalaiSathappan@users.noreply.github.com>
2021-02-04 10:01:34 +0530

    Add files via upload

---
+++ InfyGoBoot_Eureka_Server/src/test/java/com/infosys/eureka/InfyGoBootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyGoBootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
